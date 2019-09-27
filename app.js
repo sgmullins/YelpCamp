caec8fa2273b4e2a972b60776b7b879a9567aa84
Steve Mullins <sgmullins@gmail.com>
2019-09-27 18:11:28 +0000

    add databaseurl env setting

--- app.js
+++ app.js
@@ -15,16 +15,19 @@ var express     = require("express"),
 var commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index");
-    
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-mongoose.connect("mongodb+srv://sgmullins:password123456@cluster0-ynxqe.mongodb.net/test?retryWrites=true&w=majority", {
-	useNewUrlParser: true,
-	useCreateIndex: true
-}).then(() => {
-	console.log('Connected to DB!');
-}).catch(err => {
-	console.log('ERROR:', err.message);
-});
+
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+
+// mongoose.connect("mongodb+srv://sgmullins:password123456@cluster0-ynxqe.mongodb.net/test?retryWrites=true&w=majority", {
+// 	useNewUrlParser: true,
+// 	useCreateIndex: true
+// }).then(() => {
+// 	console.log('Connected to DB!');
+// }).catch(err => {
+// 	console.log('ERROR:', err.message);
+// });
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
